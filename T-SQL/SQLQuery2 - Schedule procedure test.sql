da95b62169285ce83ba13676562791f172c1fa6e
ec1yps_ <Mihli_sk06@top-academy.ru>
2025-02-27 21:41:02 +0300

    PROC ScheduleForBaseStacionarGroup in progress

--- T-SQL/SQLQuery2 - Schedule procedure test.sql
+++ T-SQL/SQLQuery2 - Schedule procedure test.sql
@@ -1,13 +1,16 @@
 USE PV_319_Import;
+SET DATEFIRST 1;
 GO
 
---EXEC sp_SetScheduleForStacionarGroup N'PD_321', N'%MS SQL Server%', N'Ковтун', N'2024-12-21', N'13:30';
+--EXEC sp_SetScheduleForStacionarGroup N'PV_319', N'%MS SQL Server%', N'Ковтун', N'2024-12-21', N'18:30';
+--EXEC sp_SetScheduleForStacionarGroup N'PD_321', N'%MS SQL Server%', N'Покидюк', N'2024-12-21', N'13:30';
 
-EXEC sp_PrintScheduleForGroup N'PD_321';
+--EXEC sp_PrintScheduleForGroup N'PD_321';
 
---EXEC sp_PrintScheduleForGroupPeriod N'PV_319', '2024-06-01', '2024-07-01';
+--EXEC sp_PrintScheduleForGroupPeriod N'', '2024-11-28', '2024-12-31';
 
-DECLARE @salary SMALLMONEY;
-EXEC @salary = sp_CountTeachersMonthSalary N'Ковтун', 2024, 06;
-PRINT (@salary);
---SELECT * FROM Schedule;
+--DECLARE @salary SMALLMONEY;
+--EXEC @salary = sp_CountTeachersMonthSalary N'Ковтун', 2024, 06;
+--PRINT (@salary);
+
+EXEC sp_ScheduleForBaseStacionarGroup N'PV_319', N'Процедурное программирование %', N'Ковтун', N'2023-11-23', N'18:30', 6, 2, 0
